0e37ec9b40ae31ef6280471698eb6ee3f2c8477a
Rafael Souza da Silva <rafael_popo@hotmail.com>
2020-09-04 15:24:23 -0300

    Aula 13 e exercicios

---
+++ JAVA-Basico/Aulas/RepeticaoFor/src/repeticaofor/RepeticaoFor.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package repeticaofor;
+
+/**
+ *
+ * @author rssilva
+ */
+public class RepeticaoFor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    //    for (int c=5; c <= 15; c++){ // c++ = incrementa + 1
+    //        System.out.println(c);
+    //    }
+    //    for (int c=15; c >= 5; c-=2){ // c-=2 = decrementa -2
+    //        System.out.println(c);
+    //    }
+        for (int c=1; c <= 100; c+=10){ // c-=2 = decrementa -2
+            System.out.println(c);
+        }
+    }
+    
+}
