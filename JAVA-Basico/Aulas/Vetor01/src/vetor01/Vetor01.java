69c2e902921be7cf5c2edb695dbb97b4c3e4cd8d
Rafael Souza da Silva <rafael_popo@hotmail.com>
2020-09-08 17:07:07 -0300

    Aula e exercicios 14

---
+++ JAVA-Basico/Aulas/Vetor01/src/vetor01/Vetor01.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vetor01;
+
+/**
+ *
+ * @author rssilva
+ */
+public class Vetor01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int num[] = {3,2,8,7,5,4};
+        System.out.println("Total de casas de N " + num.length);
+        for(int c = 0; c <=num.length - 1; c++){
+            System.out.println("Na posição " + c + " temos o valor " + num[c]);
+        }
+    }
+    
+}
