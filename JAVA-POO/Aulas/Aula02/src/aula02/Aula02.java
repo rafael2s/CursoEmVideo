a21589eeb85130ce9b35ac5896858097879b9ebf
Rafael Souza da Silva <rafael_popo@hotmail.com>
2020-09-09 11:46:24 -0300

    Visibilidade de Attr e Metodos

--- JAVA-POO/Aulas/Aula02/src/aula02/Aula02.java
+++ JAVA-POO/Aulas/Aula02/src/aula02/Aula02.java
@@ -4,7 +4,7 @@ public class Aula02 {
         Caneta c1 = new Caneta();
         c1.cor = "Azul";
         c1.ponta = 0.5f;
-        c1.tampar();
+        c1.tampar(); 
         c1.status();
         c1.rabiscar();
         
--- JAVA-POO/Aulas/Aula03/src/aula02/Caneta.java
+++ JAVA-POO/Aulas/Aula03/src/aula02/Caneta.java
@@ -1,13 +1,13 @@
 package aula02;
 
 public class Caneta {
-     String modelo;
-    String cor;
-    float ponta;
-    int carga;
-    boolean tampada;
+    public String modelo;
+    public String cor;
+    private float ponta;
+    protected int carga;
+    private boolean tampada;
     
-    void status(){
+    public void status(){
         System.out.println("Modelo: " + this.modelo);
         System.out.println("Uma caneta: " + this.cor);
         System.out.println("Ponta: " + this.ponta);
@@ -15,7 +15,7 @@ public class Caneta {
         System.out.println("Está tampada? " + this.tampada);
     }
     
-    void rabiscar() {
+    public void rabiscar() {
         if (this.tampada == true){
             System.out.println("ERRO! Não posso rabiscar");
         } else {
@@ -23,11 +23,11 @@ public class Caneta {
         }
     }
     
-    void tampar() {
+    public void tampar() {
         this.tampada = true;
     }
     
-    void destampar(){
+    public void destampar(){
         this.tampada = false;
     }
 }
